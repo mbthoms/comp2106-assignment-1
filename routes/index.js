2a605484c68fa7373568153fc1954bdd843bfa11
mbthoms <mbthoms@outlook.com>
2016-02-14 13:09:50 -0500

    Adding Services Page.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'Home - Matthew Thoms, Web Designer / Developer',
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-/* GET about page. */
+// GET about page.
 router.get('/about', function(req, res, next) {
   res.render('about', {
     title: 'About - Matthew Thoms, Web Designer / Developer',
@@ -19,7 +19,7 @@ router.get('/about', function(req, res, next) {
   });
 });
 
-/* GET projects page. */
+// GET projects page.
 router.get('/projects', function(req, res, next) {
   res.render('projects', {
     title: 'Projects - Matthew Thoms, Web Designer / Developer',
@@ -28,7 +28,16 @@ router.get('/projects', function(req, res, next) {
   });
 });
 
-/* GET contact page. */
+//GET services page.
+router.get('/services', function(req, res, next) {
+  res.render('services', {
+   title: 'Services - Matthew Thoms, Web Designer / Developer',
+    headerImage: 'images/services-header.jpg',
+    customPageStylesheet: '/stylesheets/services-nav.css'
+  });
+});
+
+// GET contact page.
 router.get('/contact', function(req, res, next) {
   res.render('contact', {
     title: 'Contact - Matthew Thoms, Web Designer / Developer',
